700c527ae08cb1bdec31cc8a81138c47b68146cf
Flo <flo@uwap.org>
2023-10-02 21:12:56 +0200

    Added a POST handler.

---
+++ POST.cs
@@ -0,0 +1,20 @@
+﻿namespace uwap.WebFramework.Plugins;
+
+public partial class MailPlugin : Plugin
+{
+    public override async Task Handle(PostRequest req, string path, string pathPrefix)
+    {
+        if (req.User == null || (!req.LoggedIn))
+        {
+            req.Status = 403;
+            return;
+        }
+
+        switch (path)
+        {
+            default:
+                req.Status = 404;
+                break;
+        }
+    }
+}
