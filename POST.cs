a045773bf71353a378780650f1111706ad025167
Flo <flo@uwap.org>
2023-10-02 21:30:33 +0200

    Added /save-draft to POST.

--- POST.cs
+++ POST.cs
@@ -1,4 +1,6 @@
-﻿namespace uwap.WebFramework.Plugins;
+﻿using uwap.WebFramework.Accounts;
+
+namespace uwap.WebFramework.Plugins;
 
 public partial class MailPlugin : Plugin
 {
@@ -12,6 +14,43 @@ public partial class MailPlugin : Plugin
 
         switch (path)
         {
+            case "/save-draft":
+                {
+                    if (InvalidMailbox(req, out var mailbox))
+                        break;
+                    if (!(req.Query.TryGetValue("to", out var toString) && req.Query.TryGetValue("subject", out var subject)))
+                    {
+                        req.Status = 400;
+                        break;
+                    }
+                    if (subject == "")
+                    {
+                        await req.Write("invalid-subject");
+                        break;
+                    }
+                    var to = toString.Split(',', ';', ' ');
+                    if (to.Any(x => !AccountManager.CheckMailAddressFormat(x)))
+                    {
+                        await req.Write("invalid-to");
+                        break;
+                    }
+                    string text = await req.GetBodyText();
+                    Directory.CreateDirectory($"../Mail/{mailbox.Id}/0");
+                    File.WriteAllText($"../Mail/{mailbox.Id}/0/text", text);
+                    mailbox.Lock();
+                    if (mailbox.Messages.TryGetValue(0, out var message))
+                    {
+                        message.From = new MailAddress(mailbox.Address, mailbox.Name ?? mailbox.Address);
+                        message.To = to.Select(x => new MailAddress(x, x)).ToList();
+                        message.Subject = subject;
+                    }
+                    else
+                    {
+                        mailbox.Messages[0] = new(new MailAddress(mailbox.Address, mailbox.Name ?? mailbox.Address), to.Select(x => new MailAddress(x, x)).ToList(), subject, null);
+                    }
+                    mailbox.UnlockSave();
+                }
+                break;
             default:
                 req.Status = 404;
                 break;
