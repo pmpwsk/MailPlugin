c63233ca9ae3f58a90f23db1abae39391b89df7a
Flo <flo@uwap.org>
2023-10-23 14:01:51 +0200

    Added MailAuthVerdictDKIM with Mixed in between Fail and Unset.

---
+++ MailAuthVerdictDKIM.cs
@@ -0,0 +1,12 @@
+﻿namespace uwap.WebFramework.Plugins;
+
+public partial class MailPlugin : Plugin
+{
+    public enum MailAuthVerdictDKIM
+    {
+        Fail = -2,
+        Mixed = -1,
+        Unset = 0,
+        Pass = 1
+    }
+}
