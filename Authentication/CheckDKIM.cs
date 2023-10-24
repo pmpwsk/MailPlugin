4dfe5b28484e6c465296859f5e08d0f340c1d737
Flo <flo@uwap.org>
2023-10-24 14:24:12 +0200

    Added a method CheckDKIM that only returns Unset (for now).

---
+++ Authentication/CheckDKIM.cs
@@ -0,0 +1,17 @@
+﻿using MimeKit;
+using MimeKit.Cryptography;
+
+namespace uwap.WebFramework.Plugins;
+
+public partial class MailPlugin : Plugin
+{
+    private static MailAuthVerdictDKIM CheckDKIM(MimeMessage message, out Dictionary<string,bool> domainResults)
+    {
+        domainResults = new();
+        var result = MailAuthVerdictDKIM.Unset;
+
+
+
+        return result;
+    }
+}
