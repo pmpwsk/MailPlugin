832969668035bd29885b0d7edb65f393476f34b7
Flo <flo@uwap.org>
2023-10-27 00:51:15 +0200

    Added DomainSelectorPair to contain a domain and selector for individual DKIM results.

---
+++ Authentication/DomainSelectorPair.cs
@@ -0,0 +1,15 @@
+﻿namespace uwap.WebFramework.Plugins;
+
+public partial class MailPlugin : Plugin
+{
+    public readonly struct DomainSelectorPair
+    {
+        public readonly string Domain, Selector;
+
+        public DomainSelectorPair(string domain, string selector)
+        {
+            Domain = domain;
+            Selector = selector;
+        }
+    }
+}
