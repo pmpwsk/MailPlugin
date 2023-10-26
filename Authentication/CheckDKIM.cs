d2adb3b4c80961785b911cb7a4e6de87e5493bad
Flo <flo@uwap.org>
2023-10-26 02:58:34 +0200

    Implemented CheckDKIM.

--- Authentication/CheckDKIM.cs
+++ Authentication/CheckDKIM.cs
@@ -12,7 +12,31 @@ public partial class MailPlugin : Plugin
             domainResults = new();
             var result = MailAuthVerdictDKIM.Unset;
 
+            var verifier = new DkimVerifier(new DkimPublicKeyLocator());
 
+            foreach (var header in message.Headers.Where(x => x.Id == HeaderId.DkimSignature))
+            {
+                var parameters = MimeKitCryptographyUser.ParseParameterTags(header.Id, header.Value);
+                MimeKitCryptographyUser.ValidateDkimSignatureParameters(parameters, out _, out _, out _, out string? domain, out _, out _, out _, out _, out _, out _);
+                if (domain == null)
+                    continue;
+                bool valid = verifier.Verify(message, header);
+                domainResults[domain] = valid;
+                switch (result)
+                {
+                    case MailAuthVerdictDKIM.Unset:
+                        result = valid ? MailAuthVerdictDKIM.Pass : MailAuthVerdictDKIM.Fail;
+                        break;
+                    case MailAuthVerdictDKIM.Pass:
+                        if (!valid)
+                            result = MailAuthVerdictDKIM.Mixed;
+                        break;
+                    case MailAuthVerdictDKIM.Fail:
+                        if (valid)
+                            result = MailAuthVerdictDKIM.Mixed;
+                        break;
+                }
+            }
 
             return result;
         }
