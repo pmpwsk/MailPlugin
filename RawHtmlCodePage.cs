50e47c4d25a28a3ae0d8c2e589b2d5f33d1553ec
Flo <flo@uwap.org>
2023-11-02 18:29:45 +0100

    Added RawHtmlCodePage.

---
+++ RawHtmlCodePage.cs
@@ -0,0 +1,19 @@
+﻿namespace uwap.WebFramework.Plugins;
+
+public partial class MailPlugin : Plugin
+{
+    private class RawHtmlCodePage : IPage
+    {
+        private readonly string Code;
+
+        public RawHtmlCodePage(string code)
+        {
+            Code = code;
+        }
+
+        public IEnumerable<string> Export(AppRequest _)
+        {
+            yield return Code;
+        }
+    }
+}
