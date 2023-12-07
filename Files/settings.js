0b523e66ee35615fa00f03714c5306e43ade37ae
Flo <flo@uwap.org>
2023-12-07 19:58:05 +0100

    Added SaveFooter and FooterChanged to settings.js.

--- Files/settings.js
+++ Files/settings.js
@@ -1,5 +1,7 @@
 let nameInput = document.querySelector("#name-input");
 let nameButton = document.querySelector("#save-name");
+let footerInput = document.querySelector("#footer-input");
+let footerButton = document.querySelector("#save-footer");
 
 function NameChanged() {
     nameButton.innerText = "Save";
@@ -23,5 +25,30 @@ async function SaveName() {
     }
     nameButton.innerText = "Save";
     nameButton.className = "green";
+    return false;
+}
+
+function FooterChanged() {
+    footerButton.innerText = "Save";
+    footerButton.className = "green";
+}
+
+async function SaveFooter() {
+    footerButton.innerText = "Saving...";
+    footerButton.className = "green";
+    try {
+        let response = await fetch("/api[PATH_PREFIX]/set-footer?mailbox=" + GetQuery("mailbox") + "&footer=" + encodeURIComponent(footerInput.value));
+        if (response.status === 200) {
+            footerButton.innerText = "Saved!";
+            footerButton.className = "";
+            return;
+        } else {
+            ShowError("Connection failed.");
+        }
+    } catch {
+        ShowError("Connection failed.");
+    }
+    footerButton.innerText = "Save";
+    footerButton.className = "green";
     return false;
 }
