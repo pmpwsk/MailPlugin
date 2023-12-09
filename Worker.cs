f64be190d01353e421cf2926d2612607cc9c89fc
Flo <flo@uwap.org>
2023-12-09 01:18:09 +0100

    Changed the worker to delete messages that are due to be deleted.

--- Worker.cs
+++ Worker.cs
@@ -5,6 +5,26 @@ public partial class MailPlugin : Plugin
     public override Task Work()
     {
         Mailboxes.RebuildAccelerators();
+
+        foreach (var mailboxKV in Mailboxes)
+        {
+            Mailbox mailbox = mailboxKV.Value;
+            List<ulong> due = [];
+            foreach (var messageKV in mailboxKV.Value.Messages)
+            {
+                DateTime? deleted = messageKV.Value.Deleted;
+                if (deleted != null && (deleted.Value + TimeSpan.FromDays(30)) <= DateTime.UtcNow)
+                    due.Add(messageKV.Key);
+            }
+            if (due.Count > 0)
+            {
+                mailbox.Lock();
+                foreach (ulong id in due)
+                    mailbox.Messages.Remove(id);
+                mailbox.UnlockSave();
+            }    
+        }
+
         return Task.CompletedTask;
     }
 }
