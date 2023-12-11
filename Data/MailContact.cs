7bd1983fec6ad80b68f747161fccb7c52b405824
Flo <flo@uwap.org>
2023-12-11 18:41:11 +0100

    Added MailContact.

---
+++ Data/MailContact.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+
+namespace uwap.WebFramework.Plugins;
+
+public partial class MailPlugin : Plugin
+{
+    [DataContract]
+    public class MailContact(string name, bool favorite = false)
+    {
+        [DataMember] public string Name = name;
+        [DataMember] public bool Favorite = favorite;
+    }
+}
