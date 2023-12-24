0cd7b8924e6b386f5cc023d0180aa8483b844c35
Flo <flo@uwap.org>
2023-12-25 00:39:09 +0100

    Added Original() to forward.js.

--- Files/forward.js
+++ Files/forward.js
@@ -6,3 +6,32 @@ async function Quote() {
         ShowError("Connection failed.");
     }
 }
+
+async function Original() {
+    let send = document.querySelector('#send');
+    send.innerText = "Sending...";
+    try {
+        let response = await fetch("/api[PATH_PREFIX]/forward/original?mailbox=" + GetQuery("mailbox") + "&folder=" + GetQuery("folder") + "&message=" + GetQuery("message") + "&info=" + document.querySelector("#info").checked + "&to=" + encodeURIComponent(document.querySelector("#to").value));
+        if (response.status === 200) {
+            let text = await response.text();
+            if (text.startsWith("message=")) {
+                window.location.assign("[PATH_HOME]?mailbox=" + GetQuery("mailbox") + "&folder=Sent&" + text);
+                return;
+            } else {
+                switch (text) {
+                    case "invalid-to":
+                        ShowError("Enter at least one recipient.");
+                        break;
+                    default:
+                        ShowError("Connection failed.");
+                        break;
+                }
+            }
+        } else {
+            ShowError("Connection failed.");
+        }
+    } catch {
+        ShowError("Connection failed.");
+    }
+    send.innerText = "Send";
+}
