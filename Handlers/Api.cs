ac06408e014a656fa06939c9bc0a70c2f4627990
Flo <flo@uwap.org>
2023-12-25 00:38:54 +0100

    Added /api/forward/original.

--- Handlers/Api.cs
+++ Handlers/Api.cs
@@ -420,6 +420,81 @@ public partial class MailPlugin : Plugin
                     mailbox.UnlockSave();
                 }
                 break;
+            case "/forward/original":
+                {
+                    if (InvalidMailboxOrMessageOrFolder(req, out var mailbox, out var originalMessage, out var originalMessageId, out _, out _))
+                        break;
+                    if (!(req.Query.TryGetValue("info", out string? infoS) && bool.TryParse(infoS, out bool info) && req.Query.TryGetValue("to", out var toString)))
+                    {
+                        req.Status = 400;
+                        break;
+                    }
+                    var to = toString.Split(',', ';', ' ').Where(x => x != "");
+                    if (to.Any(x => !AccountManager.CheckMailAddressFormat(x)))
+                    {
+                        await req.Write("invalid-to");
+                        break;
+                    }
+                    mailbox.Lock();
+                    string subject = originalMessage.Subject.Trim();
+                    while (subject.SplitAtFirst(':', out var subjectPrefix, out var realSubject) && subjectPrefix.All(char.IsLetter) && (subjectPrefix.Length == 2 || subjectPrefix.Length == 3) && realSubject.TrimStart() != "")
+                        subject = realSubject.TrimStart();
+                    MailMessage message = new(new MailAddress(mailbox.Address, mailbox.Name ?? mailbox.Address), to.Select(x => new MailAddress(x, mailbox.Contacts.TryGetValue(x, out var contact) ? contact.Name : x)).ToList(), "Fwd: " + subject, null);
+                    foreach (var attachment in originalMessage.Attachments)
+                        message.Attachments.Add(attachment);
+                    string? htmlPart = File.Exists($"../Mail/{mailbox.Id}/{originalMessageId}/html") ? File.ReadAllText($"../Mail/{mailbox.Id}/{originalMessageId}/html") : null;
+                    string? textPart = File.Exists($"../Mail/{mailbox.Id}/{originalMessageId}/text") ? File.ReadAllText($"../Mail/{mailbox.Id}/{originalMessageId}/text") : null;
+                    if (info)
+                    {
+                        string prefix = $"# Forwarded message:\n# From: {originalMessage.From.FullString}\n# Time: {DateTimeString(originalMessage.TimestampUtc)} UTC\n\n\n";
+                        if (htmlPart != null)
+                            htmlPart = prefix.Replace("\n", "<br/>") + htmlPart;
+                        if (textPart != null)
+                            textPart = prefix + textPart;
+                    }
+                    MailGen msg = new(new(message.From.Name, message.From.Address), message.To.Select(x => new MailboxAddress(x.Name, x.Address)), message.Subject, textPart, htmlPart);
+                    int counter = 0;
+                    foreach (var attachment in originalMessage.Attachments)
+                    {
+                        msg.Attachments.Add(new($"../Mail/{mailbox.Id}/{originalMessageId}/{counter}", string.IsNullOrEmpty(attachment.Name) ? "Unknown name" : attachment.Name, attachment.MimeType));
+                        counter++;
+                    }
+                    var result = MailManager.Out.Send(msg, out var messageIds);
+                    message.MessageId = string.Join('\n', messageIds);
+                    var log = message.Log;
+                    if (result.Internal.Count != 0)
+                    {
+                        log.Add("Internal:");
+                        foreach (var l in result.Internal)
+                            log.Add(l.Key.Address + ": " + l.Value);
+                    }
+                    if (result.FromSelf != null)
+                    {
+                        log.Add("From the server directly: " + result.FromSelf.ResultType.ToString());
+                        foreach (string l in result.FromSelf.ConnectionLog)
+                            log.Add(l);
+                    }
+                    if (result.FromBackup != null)
+                    {
+                        log.Add("From the backup sender: " + result.FromBackup.ResultType.ToString());
+                        foreach (string l in result.FromBackup.ConnectionLog)
+                            log.Add(l);
+                    }
+                    ulong messageId = (ulong)message.TimestampUtc.Ticks;
+                    while (mailbox.Messages.ContainsKey(messageId))
+                        messageId++;
+                    mailbox.Messages[messageId] = message;
+                    mailbox.Folders["Sent"].Add(messageId);
+                    Directory.CreateDirectory($"../Mail/{mailbox.Id}/{messageId}");
+                    for (int i = 0; i < originalMessage.Attachments.Count; i++)
+                        File.Copy($"../Mail/{mailbox.Id}/{originalMessageId}/{i}", $"../Mail/{mailbox.Id}/{messageId}/{i}");
+                    if (htmlPart != null)
+                        File.WriteAllText($"../Mail/{mailbox.Id}/{messageId}/html", htmlPart);
+                    if (textPart != null)
+                        File.WriteAllText($"../Mail/{mailbox.Id}/{messageId}/text", textPart);
+                    await req.Write("message=" + messageId);
+                    mailbox.UnlockSave();
+                } break;
             case "/move":
                 {
                     if (InvalidMailboxOrMessageOrFolder(req, out var mailbox, out _, out var messageId, out var folder, out var folderName))
